e90e05bce2c6da8e53a1629986137845702dc960
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-09-21 13:30:48 -0400

    Updating the comments in the DisplayGraphicalScene.

--- src/main/java/labfour/DisplayGraphicalScene.java
+++ src/main/java/labfour/DisplayGraphicalScene.java
@@ -7,7 +7,7 @@ import javax.swing.JFrame;
  * This class uses another class to produce simple graphical output.
  * The program will display a small graphical scene.
  *
- * <p>Bugs: This program has no known bugs.
+ * <p>Bugs: This program has no known bugs, but is missing code
  *
  * @author Gregory M. Kapfhammer and Janyl Jumadinova and Eric Knoll
  */
@@ -17,11 +17,6 @@ public class DisplayGraphicalScene {
   private static final int CANVAS_WIDTH = 600;
   private static final int CANVAS_HEIGHT = 400;
 
-  /**
-   * This is the main method that constructs the PaintGraphicalScene.
-   * This method will set the size of the canvas and then
-   * use the PaintGraphicalScene class to paint a scene on the screen.
-   */
   public static void main(String[] args) {
     JFrame window = new JFrame("Gregory M. Kapfhammer " + new Date());
     // Add the drawing canvas and do necessary things to
@@ -29,9 +24,9 @@ public class DisplayGraphicalScene {
     window.getContentPane().add(new PaintGraphicalScene());
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // make the window visible on the computer screen
-    window.setVisible(true);
+
     // set the size of the window that the program displays
-    window.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+
   }
 }
 
